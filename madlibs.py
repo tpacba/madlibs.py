57c98507ffcdd3e76e5bb3ae45bfff672843d901
Tristan Pacba <tpacba@live.com>
2021-01-06 20:33:28 -0800

    added madlibs

---
+++ madlibs.py
@@ -0,0 +1,16 @@
+# ways of string concatenation
+# user = "tristan"
+
+# print("hello " + user)
+# print("hello {}".format(user))
+# print(f"hello {user}")
+
+adj = input("Enter your adjective: ")
+verb_1 = input("Enter a verb: ")
+verb_2 = input("Enter a verb: ")
+famous_person = input("Enter a famous person: ")
+
+madlib = f"I love how computer programming is so {adj}. \
+It makes me {verb_1} more. Now I can {verb_2} like {famous_person}."
+
+print(madlib)
