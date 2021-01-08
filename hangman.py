f86db04be24072cdf80aaafed9dee9c9f4a67c3b
Tristan Pacba <tpacba@live.com>
2021-01-08 15:43:23 -0800

    added while loop for hangman, used empty set to store used letters and list to keep track

--- hangman.py
+++ hangman.py
@@ -1,5 +1,40 @@
 import json
 import random
+import string
 
-with open('words.json') as file:
-    print(random.choice(json.load(file).get("words")))
+def get_random_word():
+    with open('words.json') as file:
+        random_word = random.choice(json.load(file).get("words"))
+        return random_word
+
+def hangman():
+    word = get_random_word()
+    word_letters = set(word)
+    alphabet = set(string.ascii_lowercase)
+    used_letters = set()
+
+    print(word)
+    print(word_letters)
+
+    while len(word_letters) > 0:
+        print("You have used these letters: ", " ".join(used_letters))
+
+        word_list = [letter if letter in used_letters else "_" for letter in word]
+        print("Current word: ", " ".join(word_list))
+
+        guess = input("Guess a letter: ").lower()
+        if guess in alphabet - used_letters:
+            used_letters.add(guess)
+            if guess in word_letters:
+                word_letters.remove(guess)
+                print("Correct! \n")
+            else:
+                print("Letter not in word! \n")
+        elif guess in used_letters:
+            print("Letter already used! \n")
+        else:
+            print("Letter not in word! \n")
+    
+    print(f"Win! The word was {word}.")
+
+hangman()
